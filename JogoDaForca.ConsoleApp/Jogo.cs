4a2f622511339ab7f9cc3bc6d3c706869acaf2da
SauerPhilipi <philipisauer09@gmail.com>
2026-03-24 17:49:25 -0300

    MENU refatorado atraves de programaçao estruturada

---
+++ JogoDaForca.ConsoleApp/Jogo.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace JogoDaForca.ConsoleApp
+{
+    public static class Jogo
+    {
+           
+            
+        
+    }
+}
--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -1,10 +1,102 @@
-﻿namespace JogoDaForca.ConsoleApp
+﻿using System.Numerics;
+
+internal class Program
 {
-    internal class Program
+    static int totalDeTentativas = 0;
+    static void Main(string[] args)
+    
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            ApresentarMenu();
+
+            // Geração de número secreto
+            Random geradorDeNumeros = new Random();
+
+            int numeroSecreto = geradorDeNumeros.Next(1, 21);
+
+            // Loop de tentativas do jogo
+            for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
+            {
+                Console.Clear();
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine($"Tentativa {tentativa} de {totalDeTentativas}");
+                Console.WriteLine("----------------------------------------");
+
+                // Lógica do jogo
+                Console.Write("Digite um número entre 1 e 20: ");
+                int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+                if (numeroDigitado == numeroSecreto)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("Parabéns! Você acertou!");
+                    Console.WriteLine("----------------------------------------");
+
+                    break;
+                }
+
+                if (tentativa == totalDeTentativas)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine($"Que pena! Você usou todas as tentativas. O número era {numeroSecreto}.");
+                    Console.WriteLine("----------------------------------------");
+
+                    break;
+                }
+
+                else if (numeroDigitado > numeroSecreto)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("O número digitado é maior que o número secreto.");
+                    Console.WriteLine("----------------------------------------");
+                }
+                else
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("O número digitado é menor que o número secreto.");
+                    Console.WriteLine("----------------------------------------");
+                }
+
+                Console.WriteLine("Aperte ENTER para continuar...");
+                Console.ReadLine();
+            }
+
+            Console.Write("Deseja continuar (s/N)? ");
+
+            string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+            if (opcaoContinuar != "S")
+                break;
         }
+
+        static int ApresentarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("----------------------------------------");
+
+            // Escolha de dificuldade
+            Console.WriteLine("Escolha um nível de dificuldade:");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("1 - Fácil (10 tentativas)");
+            Console.WriteLine("2 - Normal (5 tentativas)");
+            Console.WriteLine("3 - Difícil (3 tentativas)");
+            Console.WriteLine("----------------------------------------");
+
+            Console.Write("Digite sua escolha: ");
+            string opcaoDificuldade = Console.ReadLine()!;
+
+            if (opcaoDificuldade == "1")
+                totalDeTentativas = 10;
+            else if (opcaoDificuldade == "2")
+                totalDeTentativas = 5;
+            else if (opcaoDificuldade == "3")
+                totalDeTentativas = 3;
+
+            return totalDeTentativas;
+        }
+
     }
 }
